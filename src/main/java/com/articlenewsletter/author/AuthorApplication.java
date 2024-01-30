0bd7ead9d1342c424b1e64d4dbfd462dc90e42e4
José Neilson <joseneilson13@gmail.com>
2024-01-30 01:41:51 -0300

    remove unused code

--- src/main/java/com/articlenewsletter/author/AuthorApplication.java
+++ src/main/java/com/articlenewsletter/author/AuthorApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthorApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(AuthorApplication.class, args);
 	}
 
