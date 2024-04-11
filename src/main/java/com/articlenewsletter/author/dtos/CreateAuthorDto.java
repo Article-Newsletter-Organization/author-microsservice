b2fd78f7f5468e8d359378394b0cfd2769020414
José Neilson <joseneilson13@gmail.com>
2024-04-11 17:38:25 -0300

    fix annotation for CreateAuthorDto
    - Update @NotBlank annotation to @NotNull due to the change from Long to UUID

--- src/main/java/com/articlenewsletter/author/dtos/CreateAuthorDto.java
+++ src/main/java/com/articlenewsletter/author/dtos/CreateAuthorDto.java
@@ -2,11 +2,12 @@ package com.articlenewsletter.author.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record CreateAuthorDto(@NotBlank String name,
                               @NotBlank @Email String email,
                               String bio,
-                              @NotBlank UUID userId) {
+                              @NotNull UUID userId) {
 }
